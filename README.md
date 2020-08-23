bf58ea389214aed88b14ef77a336b12fc8dabf76
EdKami <edbattousai@gmail.com>
2020-08-23 12:23:38 -0300

    Incrementando informação de download manual da biblioteca pythonpin

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ The package first_test is used to:
 Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
 
 ```bash
+pip install pythonping
+
 pip install first_test
 ```
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="first_test",
-    version="0.0.3",
+    version="0.0.6",
     author="Eduardo Tostes",
     author_email="eduardo.tostes.jf@gmail.com",
     description="Test Ping",
